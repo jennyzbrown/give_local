71204f5342e041f4fab8ad86a5f5e98c7c851980
Jenny Brown <jennyzbrown@gmail.com>
2015-01-13 13:59:54 -0500

    added account column to users as mock 'bank'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :user_name
     devise_parameter_sanitizer.for(:sign_up) << :goal
+    devise_parameter_sanitizer.for(:sign_up) << :account
   end
 
 end
---
+++ app/controllers/donates_controller.rb
@@ -0,0 +1,2 @@
+class DonatesController < ApplicationController
+end
