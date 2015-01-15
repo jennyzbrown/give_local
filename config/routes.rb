835e1a30a8cb97775b9737341b21c559ac3e7c52
Jenny Brown <jennyzbrown@gmail.com>
2015-01-15 17:42:14 -0500

    Unique links on profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :users
 
   resources :causes
 
-  resources :donates, only: [:create]
+  resources :donates
   end
