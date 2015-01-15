dd4d490b97078506f03ad190ab03783ee8a469a9
Jenny Brown <jennyzbrown@gmail.com>
2015-01-15 14:34:56 -0500

    need join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113224928) do
+ActiveRecord::Schema.define(version: 20150115193014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150113224928) do
     t.datetime "updated_at",    null: false
     t.integer  "cause_account"
     t.integer  "user_id"
+  end
 
   add_index "causes", ["user_id"], name: "index_causes_on_user_id", using: :btree
 
