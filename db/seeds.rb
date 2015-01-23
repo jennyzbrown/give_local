9a1c5bac48cc4623feb6de80dd8ea664f5c6e4cf
Jenny Brown <jennyzbrown@gmail.com>
2015-01-22 20:48:54 -0500

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cause.create(name: 'Test', description: 'test description', street: 'Beverly St.', city: 'Boston', state: 'MA', zip: '02114', category: 'health', phone: '6038179676', url: 'www.test.com')
