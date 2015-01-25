6a5c1c24b91df06699f4d848b60b46cddd288ffb
Jenny Brown <jennyzbrown@gmail.com>
2015-01-25 17:13:39 -0500

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Cause.create(name: 'Pine Street Inn', description: 'Facilitates rescue and distr
 
 Cause.create(name: 'Project Bread', description: 'Contributes to fighting hunger.',  phone: '(617) 723-5000', category: 'food', street: '145 Border St.', city: 'Boston', state: 'MA', zip: '02128', url: 'www.projectbread.org',  image: 'food.png', cause_account: 0)
 
-Cause.create(name: 'The Nature Conservancy', description: 'Addresses conservation threats.',  phone: '(617) 532-8300', category: 'enviroment', street: '99 Bedford St.', city: 'Boston', state: 'MA', zip: '02111', url: 'http://www.nature.org',  image: 'environment.png')
+Cause.create(name: 'The Nature Conservancy', description: 'Addresses conservation threats.',  phone: '(617) 532-8300', category: 'enviroment', street: '99 Bedford St.', city: 'Boston', state: 'MA', zip: '02111', url: 'www.nature.org',  image: 'environment.png')
 
 Cause.create(name: 'Mass. Society for Prevention of Cruelty to Children', description: 'Provides services to ensure the health and safety of children',  phone: '(617) 587-1500', category: 'build', street: '3815 Washington St.', city: 'Roslindale', state: 'MA', zip: '02130', url: 'www.mspcc.com',  image: 'build.png', cause_account: 0)
 
@@ -11,17 +11,17 @@ Cause.create(name: 'Greater Boston Food Bank', description: 'Provides meals for 
 
 Cause.create(name: 'Habitat for Humanity Boston', description: 'Helps create homes for Bostonians in need', phone: '(617) 423-2223', category: 'build', street: '240 Commercial St', city: 'Boston', state: 'MA', zip: '02109’', url: 'www.habitatboston.org', image: 'build.png', cause_account: 0)
 
-Cause.create(name: 'Minutes for Memories', description: 'Helps children who have sustained long-term injuries.',  phone: '(617) 581-3116', category: 'health', street: '61 Selkirk Rd', city: 'Boston', state: 'MA', zip: '02135', url: 'www.minutesformemories.org/past-memories', image: 'health.png', cause_account: 0)
+Cause.create(name: 'Minutes for Memories', description: 'Helps children who have sustained long-term injuries.',  phone: '(617) 581-3116', category: 'health', street: '61 Selkirk Rd', city: 'Boston', state: 'MA', zip: '02135', url: 'www.minutesformemories.org', image: 'health.png', cause_account: 0)
 
 Cause.create(name: 'Community Servings', description: 'Delivers meals to those who need them',  phone: '(617) 522-7777', category: 'food', street: '18 Marbury Ter.', city: 'Boston', state: 'MA', zip: 02130, url: 'www.servings.org',  image: 'food.png', cause_account: 0)
 
 
-Cause.create(name: 'Photos for Cures', description: 'Raises money for St. Judes one photo at a time',  phone: '6038179676', category: 'health', street: 'Beverly St.', city: 'Boston', state: 'MA', zip: '02114', url: 'www.test.com',  image: 'health.png', cause_account: 0)
+Cause.create(name: 'AIDS Action Committee of Mass', description: 'Provides services to people suffering from HIV/AIDS',  phone: '(800) 235-2331', category: 'health', street: '75 Amory St.', city: 'Boston', state: 'MA', zip: '02119', url: 'www.aac.org',  image: 'health.png', cause_account: 0)
 
 Cause.create(name: 'City Sprouts', description: 'Develops and maintains local gardens for communities and schools',  phone: '(617) 876-2436', category: 'environment', street: '678 Mass Ave.', city: 'Boston', state: 'MA', zip: '02139', url: 'www.citysprouts.org',  image: 'environment.png', cause_account: 0)
 
 Cause.create(name: 'Lovin Spoonfuls', description: 'Distributes healthy meals.',  phone: '(617) 390-4450', category: 'food', street: '418 Commonwealth Ave.', city: 'Boston', state: 'MA', zip: '02215', url: 'www.lovinspoonfulsinc.org', image: 'food.png', cause_account: 0)
 
-Cause.create(name: 'Cradles to Crayons', description: 'Provides essentials for children who are homeless or struggling.',  phone: '(617) 390-4450', category: 'shelter', street: '155 North Beacon Street St.', city: 'Boston', state: 'MA', zip: '02135', url: 'cradlestocrayons.org', image: 'shelter.png', cause_account: 0)
+Cause.create(name: 'Cradles to Crayons', description: 'Provides essentials for children who are homeless or struggling.',  phone: '(617) 390-4450', category: 'shelter', street: '155 North Beacon Street St.', city: 'Boston', state: 'MA', zip: '02135', url: 'www.cradlestocrayons.org', image: 'shelter.png', cause_account: 0)
 
 
